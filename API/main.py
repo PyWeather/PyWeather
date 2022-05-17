6986f2fdd24d448f8af77a96ea70c784de131695
jjay31 <michalwojtowicz_2001@tlen.pl>
2022-05-18 01:34:47 +0200

    cosmetic changes

--- API/main.py
+++ API/main.py
@@ -8,7 +8,7 @@ data = CurrentWeather("PL", "Zielona Góra", api_key)
 print(f"{data.city}:\n"
       f"Data i godzina: {data.date_time}\n"
       f"Temperatura: {data.temperature}\n"
-      f"Odczuwalna temperatora: {data.feels_like}\n"
+      f"Odczuwalna temperatura: {data.feels_like}\n"
       f"Wilgotność powietrza: {data.humidity}\n"
       f"Prędkość wiatru: {data.wind_speed}\n"
       f"Pogoda: {data.description.capitalize()}")

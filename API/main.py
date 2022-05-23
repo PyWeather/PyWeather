eedf8e00910230efd1eabb1f37f46785106dae5d
jjay31 <michalwojtowicz_2001@tlen.pl>
2022-05-23 02:15:32 +0200

    add ThreeHourForecast test

--- API/main.py
+++ API/main.py
@@ -1,7 +1,7 @@
 from CurrentWeather import CurrentWeather
+from ThreeHourForecast import ThreeHourForecast
 
 api_key = "e3b4d61af9466bfa61b7d81adff527ba"
-
 
 data = CurrentWeather("PL", "Zielona Góra", api_key)
 
@@ -12,3 +12,13 @@ print(f"{data.city}:\n"
       f"Wilgotność powietrza: {data.humidity}\n"
       f"Prędkość wiatru: {data.wind_speed}\n"
       f"Pogoda: {data.description.capitalize()}")
+
+data2 = ThreeHourForecast("PL", "Zielona Góra", api_key)
+
+print(f"{data2.parsed_data[0].city}:\n"
+      f"Data i godzina: {data2.parsed_data[0].date}\n"
+      f"Temperatura: {data2.parsed_data[0].temperature}\n"
+      f"Odczuwalna temperatura: {data2.parsed_data[0].feels_like}\n"
+      f"Wilgotność powietrza: {data2.parsed_data[0].humidity}\n"
+      f"Prędkość wiatru: {data2.parsed_data[0].wind_speed}\n"
+      f"Pogoda: {data2.parsed_data[0].description.capitalize()}")

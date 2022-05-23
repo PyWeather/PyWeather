bfe5a5fb01dd2e12eacb65f9a1f7e6f4615e98fd
jjay31 <michalwojtowicz_2001@tlen.pl>
2022-05-23 19:04:58 +0200

    add daily forecast

---
+++ API/DailyForecast.py
@@ -0,0 +1,34 @@
+import requests
+from datetime import datetime
+from typing import List, Dict
+from WeatherData import WeatherData
+
+
+class DailyForecast(object):
+    def __init__(self, country: str, city: str, api_key: str, units: str = 'metric', lang: str = 'pl'):
+        lat_lon_url: str = f"https://api.openweathermap.org/geo/1.0/direct?q={city},{country}&limit=1&appid={api_key}"
+        lat_lon: Dict = requests.get(lat_lon_url).json()
+        forecast_url: str = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat_lon[0]['lat']}" \
+                            f"&lon={lat_lon[0]['lon']}&appid={api_key}&units={units}&lang={lang}"
+        data: Dict = requests.get(forecast_url).json()
+        self.list_of_weather_data: List[Dict] = data["list"]
+        self.parsed_day_data: List[WeatherData] = [WeatherData(city,
+                                                   datetime.utcfromtimestamp(x["dt"] + 7200)
+                                                   .strftime('%d-%m-%Yr. %H:%M:%S'),
+                                                   x["main"]["temp"],
+                                                   x["main"]["feels_like"],
+                                                   x["main"]["humidity"],
+                                                   x["wind"]["speed"],
+                                                   x["weather"][0]["description"])
+                                                   for x in self.list_of_weather_data
+                                                   if datetime.utcfromtimestamp(x["dt"] + 7200).strftime('%H') == "14"]
+        self.parsed_night_data: List[WeatherData] = [WeatherData(city,
+                                                                 datetime.utcfromtimestamp(x["dt"] + 7200)
+                                                                 .strftime('%d-%m-%Yr. %H:%M:%S'),
+                                                                 x["main"]["temp"],
+                                                                 x["main"]["feels_like"],
+                                                                 x["main"]["humidity"],
+                                                                 x["wind"]["speed"],
+                                                                 x["weather"][0]["description"])
+                                                     for x in self.list_of_weather_data
+                                                     if datetime.utcfromtimestamp(x["dt"] + 7200).strftime('%H') == "02"]
--- API/usage.py
+++ API/usage.py
@@ -1,5 +1,6 @@
 from CurrentWeather import CurrentWeather
 from ThreeHourForecast import ThreeHourForecast
+from DailyForecast import DailyForecast
 
 api_key = "e3b4d61af9466bfa61b7d81adff527ba"
 
@@ -26,3 +27,25 @@ print(f"{data2.parsed_data[0].city}:\n"
       f"Wilgotność powietrza: {data2.parsed_data[0].humidity}\n"
       f"Prędkość wiatru: {data2.parsed_data[0].wind_speed}\n"
       f"Pogoda: {data2.parsed_data[0].description.capitalize()}")
+
+print("\n=================\n")
+
+data3 = DailyForecast("PL", "Zielona Góra", api_key)
+
+for i in data3.parsed_day_data:
+    print(f"{i.city}:\n"
+          f"Data i godzina: {i.date}\n"
+          f"Temperatura: {i.temperature}\n"
+          f"Odczuwalna temperatura: {i.feels_like}\n"
+          f"Wilgotność powietrza: {i.humidity}\n"
+          f"Prędkość wiatru: {i.wind_speed}\n"
+          f"Pogoda: {i.description.capitalize()}\n")
+
+for i in data3.parsed_night_data:
+    print(f"{i.city}:\n"
+          f"Data i godzina: {i.date}\n"
+          f"Temperatura: {i.temperature}\n"
+          f"Odczuwalna temperatura: {i.feels_like}\n"
+          f"Wilgotność powietrza: {i.humidity}\n"
+          f"Prędkość wiatru: {i.wind_speed}\n"
+          f"Pogoda: {i.description.capitalize()}\n")

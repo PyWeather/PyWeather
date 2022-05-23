2da93b58ae28a7969427a790ef66387d611afb17
jjay31 <michalwojtowicz_2001@tlen.pl>
2022-05-23 19:06:39 +0200

    cosmetic changes

--- API/usage.py
+++ API/usage.py
@@ -4,35 +4,35 @@ from DailyForecast import DailyForecast
 
 api_key = "e3b4d61af9466bfa61b7d81adff527ba"
 
-data = CurrentWeather("PL", "Zielona Góra", api_key)
+current_weather = CurrentWeather("PL", "Zielona Góra", api_key)
 
-print(f"{data.city}:\n"
-      f"Data i godzina: {data.date_time}\n"
-      f"Temperatura: {data.temperature}\n"
-      f"Odczuwalna temperatura: {data.feels_like}\n"
-      f"Wilgotność powietrza: {data.humidity}\n"
-      f"Prędkość wiatru: {data.wind_speed}\n"
-      f"Pogoda: {data.description.capitalize()}\n"
-      f"Wschód słońca: {data.sunrise}\n"
-      f"Zachód słońca: {data.sunset}")
-
-print("\n=================\n")
-
-data2 = ThreeHourForecast("PL", "Zielona Góra", api_key)
-
-print(f"{data2.parsed_data[0].city}:\n"
-      f"Data i godzina: {data2.parsed_data[0].date}\n"
-      f"Temperatura: {data2.parsed_data[0].temperature}\n"
-      f"Odczuwalna temperatura: {data2.parsed_data[0].feels_like}\n"
-      f"Wilgotność powietrza: {data2.parsed_data[0].humidity}\n"
-      f"Prędkość wiatru: {data2.parsed_data[0].wind_speed}\n"
-      f"Pogoda: {data2.parsed_data[0].description.capitalize()}")
+print(f"{current_weather.city}:\n"
+      f"Data i godzina: {current_weather.date_time}\n"
+      f"Temperatura: {current_weather.temperature}\n"
+      f"Odczuwalna temperatura: {current_weather.feels_like}\n"
+      f"Wilgotność powietrza: {current_weather.humidity}\n"
+      f"Prędkość wiatru: {current_weather.wind_speed}\n"
+      f"Pogoda: {current_weather.description.capitalize()}\n"
+      f"Wschód słońca: {current_weather.sunrise}\n"
+      f"Zachód słońca: {current_weather.sunset}")
 
 print("\n=================\n")
 
-data3 = DailyForecast("PL", "Zielona Góra", api_key)
+three_hour_forecast = ThreeHourForecast("PL", "Zielona Góra", api_key)
 
-for i in data3.parsed_day_data:
+print(f"{three_hour_forecast.parsed_data[0].city}:\n"
+      f"Data i godzina: {three_hour_forecast.parsed_data[0].date}\n"
+      f"Temperatura: {three_hour_forecast.parsed_data[0].temperature}\n"
+      f"Odczuwalna temperatura: {three_hour_forecast.parsed_data[0].feels_like}\n"
+      f"Wilgotność powietrza: {three_hour_forecast.parsed_data[0].humidity}\n"
+      f"Prędkość wiatru: {three_hour_forecast.parsed_data[0].wind_speed}\n"
+      f"Pogoda: {three_hour_forecast.parsed_data[0].description.capitalize()}")
+
+print("\n=================\n")
+
+daily_forecast = DailyForecast("PL", "Zielona Góra", api_key)
+
+for i in daily_forecast.parsed_day_data:
     print(f"{i.city}:\n"
           f"Data i godzina: {i.date}\n"
           f"Temperatura: {i.temperature}\n"
@@ -41,7 +41,7 @@ for i in data3.parsed_day_data:
           f"Prędkość wiatru: {i.wind_speed}\n"
           f"Pogoda: {i.description.capitalize()}\n")
 
-for i in data3.parsed_night_data:
+for i in daily_forecast.parsed_night_data:
     print(f"{i.city}:\n"
           f"Data i godzina: {i.date}\n"
           f"Temperatura: {i.temperature}\n"
